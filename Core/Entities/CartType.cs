e40a7180f737518f601a5e538891b21c86bf6470
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-03-20 12:48:33 -0600

    feat: entidad cartType

---
+++ Core/Entities/CartType.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Entities
+{
+    public class CartType
+    {
+        public int Id { get; set; }
+
+        public string TypeName { get; set; } = string.Empty;
+
+        public virtual ICollection<CartLog> CartLogs { get; set; } = new List<CartLog>();
+    }
+}
