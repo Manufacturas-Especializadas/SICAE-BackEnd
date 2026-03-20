7baaee8715903e2d2d7004a778cfd48c4f972fff
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-03-20 12:49:28 -0600

    feat: servicio cart

---
+++ Application/Services/CartService.cs
@@ -0,0 +1,34 @@
+﻿using Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Application.DTOs.DTOs;
+
+namespace Application.Services
+{
+    public class CartService
+    {
+        private readonly ICartRepository _repository;
+
+        public CartService(ICartRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<IEnumerable<CartHistoryDto>> GetHistoryAsync()
+        {
+            var logs = await _repository.GetAllAsync();
+
+            return logs.Select(l => new CartHistoryDto(
+                l.Id,
+                l.Folio,
+                l.CartType?.TypeName ?? "N/A",
+                l.EntryDate,
+                l.ExitDate,
+                l.Status.ToString()
+            ));
+        }
+    }
+}
